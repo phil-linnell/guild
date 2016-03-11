d15c47425700ec5907988cfbcdd08ff3138ec83e
Phil Linnell <phil@ustwo.com>
2016-03-11 19:41:00 +0000

    Survive

--- src/data/db2.js
+++ src/data/db2.js
@@ -41,6 +41,11 @@ const eventsList = [{
   "gameName": "Jamaica",
   "players": ["0001", "0003", "0004", "0005", "0006", "0007"],
   "winner": "0007"
+},{
+  "id": "0005",
+  "gameName": "Survive: Escape from Atlantis",
+  "players": ["0001", "0003", "0004", "0005", "0006", "0007"],
+  "winner": "0005"
 }];
 
 
@@ -53,10 +58,7 @@ Kyle, Birds, yellow 80
 Dom, Arachnids, red 75
 Kelv, Mammals, white 72
 Rob, Reptiles, black 49
-*/
 
-
-/*
 Jam
 Rob, red, 40
 Phil, Green, 37
@@ -64,6 +66,18 @@ Dom, Yellow, 15
 Kyle, pink 25
 Kelv, blue, 26
 Ben, black, 10
+
+Survive
+Rob, Green, 4
+Phil, Orange, 5
+Dom, Yellow, 5
+Kyle, Red 12
+Kelv, blue, 2
+Ben, White, 1
+
+Codenames
+Team Blue: 
+Team Red:
 */
 
 
