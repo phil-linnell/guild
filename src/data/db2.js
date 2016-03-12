86a8014c14fec4000dfc81741bac785834e9e100
Phil Linnell <phil@ustwo.com>
2016-03-12 10:20:47 +0000

    checkpoint

--- src/data/db2.js
+++ src/data/db2.js
@@ -19,33 +19,53 @@ const usersList = [{
 },{
   "id": "0007",
   "name": "Rob"
+},{
+  "id": "0008",
+  "name": "Mike"
 }]
 
 const eventsList = [{
   "id": "0001",
   "gameName": "The Castles of Burgundy",
   "players": ["0001", "0002"],
-  "winner": "0001"
+  "winner": "0001",
+  "date": "2016-03-01"
 },{
   "id": "0002",
   "gameName": "Twilight Struggle",
   "players": ["0001", "0003"],
-  "winner": "0003"
+  "winner": "0003",
+  "date": "2016-03-09"
 },{
   "id": "0003",
   "gameName": "Dominant Species",
   "players": ["0001", "0003", "0004", "0005", "0006", "0007"],
-  "winner": "0001"
+  "winner": "0001",
+  "date": "2016-03-11"
 },{
   "id": "0004",
   "gameName": "Jamaica",
   "players": ["0001", "0003", "0004", "0005", "0006", "0007"],
-  "winner": "0007"
+  "winner": "0007",
+  "date": "2016-03-11"
 },{
   "id": "0005",
   "gameName": "Survive: Escape from Atlantis",
   "players": ["0001", "0003", "0004", "0005", "0006", "0007"],
-  "winner": "0005"
+  "winner": "0005",
+  "date": "2016-03-11"
+},{
+  "id": "0006",
+  "gameName": "7 Wonders",
+  "players": ["0001", "0003", "0004", "0005", "0006", "0007", "0008"],
+  "winner": "0008",
+  "date": "2016-03-11"
+},{
+  "id": "0007",
+  "gameName": "Cash n Guns",
+  "players": ["0001", "0003", "0004", "0005", "0006", "0007", "0008"],
+  "winner": "0003",
+  "date": "2016-03-11"
 }];
 
 
@@ -75,9 +95,22 @@ Kyle, Red 12
 Kelv, blue, 2
 Ben, White, 1
 
-Codenames
-Team Blue: 
-Team Red:
+7 Wonders
+Phil Babylon 55
+Kelv Ephesos 38
+Dom Olympos 52
+Kyle Rhodes 33
+Rob Giza 52
+Mike Alexandria 61
+Ben Halikarnassos 34
+
+Cash n Guns
+Dom
+Kyle
+Phil
+Ben
+Mike
+Did not finish Kelv and Rob
 */
 
 
